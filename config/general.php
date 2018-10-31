303f20d162ec11477f2bd7a997d02ec199dc8242
FredAmstutz <fred4892@gmail.com>
2018-10-31 17:36:06 -0400

    Initial commit.

---
+++ config/general.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * General Configuration
+ *
+ * All of your system's general configuration settings go in here. You can see a
+ * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
+ *
+ * @see craft\config\GeneralConfig
+ */
+
+return [
+    // Global settings
+    '*' => [
+        'defaultWeekStartDay' => 0,
+        'enableCsrfProtection' => true,
+        'omitScriptNameInUrls' => true,
+        'cpTrigger' => 'admin',
+        'securityKey' => getenv('SECURITY_KEY'),
+        'maxUploadFileSize' => 67108864, // 64MB
+        'assetHost' => getenv('ASSET_HOST')
+    ],
+    'dev' => [
+        'siteUrl' => null,
+        'devMode' => true,
+    ],
+    'staging' => [
+        'siteUrl' => null,
+    ],
+    'production' => [
+        'siteUrl' => null,
+    ],
+];
---
+++ config/volumes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	's3' => [
+		'hasUrls' => true,
+		'url' => 'https://'.getenv('S3_BUCKET').'.s3.amazonaws.com/',
+		'keyId' => getenv('S3_API_KEY'),
+		'secret' => getenv('S3_SECRET'),
+		'bucket' => getenv('S3_BUCKET'),
+		'region' => getenv('S3_REGION'),
+	],
+];
