203d83c264cbd517a2ef1fbbed193b7278fd8a87
FredAmstutz <fred4892@gmail.com>
2019-03-05 10:50:55 -0500

    Added linked scrolling functionality to section titles

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -1,3 +1,23 @@
+import scrollTo from '../utils/scrollTo';
+
+const SCROLL_DURATION = 1250;
+
+const onClickLink = event => {
+	const tocHeight = document.querySelector('.toc').offsetHeight;
+	const scrollTarget = document.querySelector(event.target.hash);
+	
+	if (!scrollTarget) return;
+
+	event.preventDefault();
+
+	// Scroll to target; conditional logic is meant to add dynamic to stop scroll before titles clip
+	if (tocHeight < 60) {
+		scrollTo(scrollTarget.offsetTop - tocHeight, SCROLL_DURATION);
+	} else {
+		scrollTo(scrollTarget.offsetTop - (tocHeight + 60), SCROLL_DURATION);
+	}
+};
+
 export default ({
 	mainContentArea,
 	proposalSections,
@@ -6,6 +26,15 @@ export default ({
 	tocTitles,
 	scrollingTocTitles,
 }) => {
+
+	tocTitles.forEach(function(link) {
+		link.addEventListener('click', onClickLink);
+	});
+
+	scrollingTocTitles.forEach(function(link) {
+		link.addEventListener('click', onClickLink);
+	});
+
 	function onScroll() {
 		// This hides and shows the section titles to mimic the stacking effect
 		for (let i = 0; i < proposalSections.length; i++) {
